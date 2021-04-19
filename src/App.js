1fb429166b3788b3cacf94385b270a6725710439
Rishab Kumar <rishabkumar7@gmail.com>
2021-04-18 22:56:46 -0400

    adde coin component and the api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Coin from "./components/coinItem/Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then((res) => {
+        setCoins(res.data)
+        console.log(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <div className="header">
@@ -15,6 +34,22 @@ function App() {
             placeholder="Search a Coin"
           />
         </form>
+      </div>
+      <div className="coinsContainer">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              volume={coin.total_volume}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
       </div>
     </div>
   );
